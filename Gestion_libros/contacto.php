8456056425976617c35e5a51e25088f512e941a8
Jessica98-san <jessanchezugsina98@gmail.com>
2025-02-13 14:21:34 -0500

    Create contacto
    Create sección contacto , validacion de datos.JS

---
+++ Gestion_libros/contacto.php
@@ -0,0 +1,37 @@
+<?php include 'header.php'; ?>
+<!Doctype html>
+<html lang='en'>
+<head>
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+<!-- Boxicons CSS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<title>Contacto</title>
+</head>
+<body>
+
+<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+
+    <div class="contenedor">
+        <adress>
+    
+            <i class='bx bxs-user-circle' style='color:rgba(255, 0, 255)' ></i> 
+            <h3>Sobre Nosotros</h3>
+            <p>
+            Somos estudiantes de la universidad de la "ESPE" 
+            </p>
+            <br>
+            <i class='bx bx-message-rounded' style='color:rgba(0, 255, 0)' ></i> 
+               <h3>Datos de contacto</h3>
+                 <h4>Teléfono</h4>
+                  <p>0979446345/098981027</p>
+                  <p>0967853471</p>
+                  <br>
+            <i class='bx bx-envelope' style='color:rgba(0, 128, 0)' ></i>
+               <h3>e-mail</h3>
+                 <e-mail>jesanchez31@espe.edu.ec</e-mail>
+        </adress>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
--- Gestion_libros/header.php
+++ Gestion_libros/header.php
@@ -23,7 +23,7 @@
                     <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="registro.php">Registrar Libro</a></li>
                     <li class="nav-item"><a class="nav-link" href="listado.php">Listado de Libros</a></li>
-
+                    <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
                 </ul>
             </div>
         </div>
