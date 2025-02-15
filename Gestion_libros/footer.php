689bd2848261b246d4a4f787b5e8590c86f98d8e
MelanieT22 <matalavera@espe.edu.ec>
2025-02-14 23:00:30 -0500

    Update footer.php

--- Gestion_libros/footer.php
+++ Gestion_libros/footer.php
@@ -1,2 +1,17 @@
+<footer class="bg-dark text-white text-center py-1 fixed-bottom" style="font-size: 0.85rem;">
+  <div class="container">
+    <p class="mb-0">&copy; 2025 Sistema Gestor de Libros. Derechos reservados.</p>
+    <div class="mt-1">
+      <a href="#" class="text-white me-2"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="text-white me-2"><i class="bi bi-twitter"></i></a>
+      <a href="#" class="text-white"><i class="bi bi-instagram"></i></a>
+    </div>
+  </div>
+</footer>
+
 <script src="script.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous"></script>
+</body>
+</html>
